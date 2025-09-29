447ab75d25e358c2a1c97dc1af88738d48cd2a2d
Andre Pretorius <pretorius.dre+github@gmail.com>
2025-09-29 18:28:23 +0800

    Bux fix on autologin middleware

--- share_dinkum_proj/share_dinkum_app/middleware.py
+++ share_dinkum_proj/share_dinkum_app/middleware.py
@@ -4,9 +4,10 @@ from django.contrib.auth import get_user_model, login
 class AutoLoginMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
-        self.user = get_user_model().objects.filter(is_superuser=True).first()
 
     def __call__(self, request):
-        if not request.user.is_authenticated and self.user:
-            login(request, self.user)
+        if not request.user.is_authenticated:
+            user = get_user_model().objects.filter(is_superuser=True).first()
+            if user:
+                login(request, user)
         return self.get_response(request)
